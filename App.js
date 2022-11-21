38f733242fe25899bf3eed1726b14a68bd97807e
Agustin Gomez Urrutia <agustin.gu20@gmail.com>
2022-11-21 18:51:47 -0300

    Seteo de estado global de beneficio seleccionado

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import { Provider } from 'react-redux';
 import { View } from 'react-native';
 import { store } from './src/store/store';
 import { styles } from './appStyles';
-// import CarouselCard from './src/components/CarouselCard/CarouselCard';
+import CarouselCard from './src/components/CarouselCard/CarouselCard';
 import BenefitScreen from './src/screens/BenefitScreen/BenefitScreen';
 import { benefitsArray } from './src/helpers/benefitsArray';
 
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <View style={styles.container}>
-        {/* <CarouselCard /> */}
+        <CarouselCard />
         <BenefitScreen {...benefitsArray} />
       </View>
     </Provider>
