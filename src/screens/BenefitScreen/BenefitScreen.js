25a2d0dd18db9e05af46339eabf0e5adafd0c8c0
Agustin Gomez Urrutia <agustin.gu20@gmail.com>
2022-11-21 19:51:52 -0300

    Se elimino la linea characters de la formula de generacion de codigo

--- src/screens/BenefitScreen/BenefitScreen.js
+++ src/screens/BenefitScreen/BenefitScreen.js
@@ -9,7 +9,6 @@ const BenefitScreen = () => {
 
   useEffect(() => {
     const generateRandomString = (num) => {
-      const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
       const result = Math.random().toString(36).substring(0, num);
       return result;
     };
