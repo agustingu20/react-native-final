4c2b9ffe130ce236fd81df473f997a1428c01811
unknown <Dan1el-aguilar>
2022-11-22 19:31:51 -0300

    register--create component register

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { View } from 'react-native';
 import { styles } from './appStyles';
 import CarouselCard from './src/components/CarouselCard/CarouselCard';
+import { RegisterUser } from './src/components/RegisterUser';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <CarouselCard />
+      <RegisterUser />
     </View>
   );
 }
