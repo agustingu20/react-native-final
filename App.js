e9c5b9ec3894b1d9281ba9bbdae1c3347d64762e
Agustin Gomez Urrutia <agustin.gu20@gmail.com>
2022-12-01 18:24:38 -0300

    Cambio en el estado de isStaff

--- App.js
+++ App.js
@@ -26,7 +26,7 @@ export default function App() {
         <View>
           <LogIn />
           {/* <RegisterUser /> */}
-        </View>
+        </View> 
         <View>
           <UserProfileScreen />
         </View>
