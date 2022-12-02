3288d5c23e49323383da124292dd79a59fda6612
Agustin Gomez Urrutia <agustin.gu20@gmail.com>
2022-12-02 20:46:53 -0300

    Fix errors RegisterUser

--- src/components/RegisterUser/RegisterUser.js
+++ src/components/RegisterUser/RegisterUser.js
@@ -1,115 +1,151 @@
-import React, { useState } from 'react';
+import React from 'react';
 import {
-  Button,
-  SafeAreaView, Text, TextInput, View,
+  Button, SafeAreaView, Text, TextInput, View,
 } from 'react-native';
-import { useForm, Controller } from 'react-hook-form'
-import { createUserWithEmailAndPassword, getAuth  } from 'firebase/auth'
+import { useForm, Controller } from 'react-hook-form';
+import { createUserWithEmailAndPassword, getAuth } from 'firebase/auth';
 import { styles } from './RegisterUserStyles';
-import app from '../../../firebase'
+import app from '../../../firebase';
 
 const RegisterUser = () => {
-  const { control, handleSubmit, formState: { errors } } = useForm()
-  const auth = getAuth(app)
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+  const auth = getAuth(app);
   const defaultValues = {
-    name: "",
-    email: "",
-    password: "",
-    password2: "",
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
   };
   const submit = async (values) => {
     try {
-      if(values.password !== values.password2) return alert('las contraseñas no coinciden')
-      const userCredential = await createUserWithEmailAndPassword(auth, values.email, values.password)
-      const { user } = userCredential
+      if (values.password !== values.password2) return alert('las contraseñas no coinciden');
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        values.email,
+        values.password,
+      );
+      const { user } = userCredential;
       console.log(user);
     } catch (error) {
       console.log(error);
     }
-   }
+  };
 
   return (
     <View style={styles.containerR}>
       <Text style={styles.title}>Ingresa tus Datos</Text>
-       <View style={styles.containerInputs}>
+      <View style={styles.containerInputs}>
         <SafeAreaView>
           <Controller
             control={control}
-            rules= {{required: true, maxLength: 25}}
-             render={({field: {onChange, onBlur, value}}) => (
+            rules={{ required: true, maxLength: 25 }}
+            render={({ field: { onChange, onBlur, value } }) => (
               <>
                 <Text>Nombre</Text>
-                <TextInput style={styles.input}
-                onBlur={onBlur}
-                onChangeText={onChange}
-                value={value}/>
-                
+                <TextInput
+                  style={styles.input}
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
               </>
-              )}
-             name="name"
-             defaultValue={defaultValues.name}
-            />
-            { errors.name?.type === 'required' && <Text style={styles.errorMsg}>nombre requerido</Text> }
-            { errors.name?.type === 'maxLength' && <Text style={styles.errorMsg}>nombre maximo 25 caracteres</Text> }
-            <Controller
+            )}
+            name="name"
+            defaultValue={defaultValues.name}
+          />
+          {errors.name?.type === 'required' && (
+            <Text style={styles.errorMsg}>nombre requerido</Text>
+          )}
+          {errors.name?.type === 'maxLength' && (
+            <Text style={styles.errorMsg}>nombre maximo 25 caracteres</Text>
+          )}
+          <Controller
             control={control}
-            rules={{required: true, pattern: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/ }}
-             render={({field: {onChange, onBlur, value}}) => (
+            rules={{
+              required: true,
+              pattern: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/,
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
               <>
                 <Text>Email</Text>
-                <TextInput style={styles.input}
-                onBlur={onBlur}
-                onChangeText={onChange}
-                value={value}/>
+                <TextInput
+                  style={styles.input}
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
               </>
-              )}
-             name="email"
-             defaultValue={defaultValues.email}
-            />
-            { errors.email?.type === 'pattern' && <Text style={styles.errorMsg}>ingrese un mail válido</Text> }
-            { errors.email?.type === 'required' && <Text style={styles.errorMsg}>campo email requerido</Text> }
-            <Controller
+            )}
+            name="email"
+            defaultValue={defaultValues.email}
+          />
+          {errors.email?.type === 'pattern' && (
+            <Text style={styles.errorMsg}>ingrese un mail válido</Text>
+          )}
+          {errors.email?.type === 'required' && (
+            <Text style={styles.errorMsg}>campo email requerido</Text>
+          )}
+          <Controller
             control={control}
-            rules={{required: true, maxLength: 25, minLength: 8}}
-             render={({field: {onChange, onBlur, value}}) => (
+            rules={{ required: true, maxLength: 25, minLength: 8 }}
+            render={({ field: { onChange, onBlur, value } }) => (
               <>
                 <Text>Contraseña</Text>
-                <TextInput style={styles.input}
-                onBlur={onBlur}
-                onChangeText={onChange}
-                value={value}
-                secureTextEntry={true}/>
+                <TextInput
+                  style={styles.input}
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  secureTextEntry={true}
+                />
               </>
-              )}
-             name="password"
-             defaultValue={defaultValues.password}
-            />
-             { errors.password?.type === 'required' && <Text style={styles.errorMsg}>Contraseña requerida</Text> }
-             { errors.password?.type === 'minLength' && <Text style={styles.errorMsg}>Contraseña minimo 8 caracteres</Text> }
-             { errors.password?.type === 'maxLength' && <Text style={styles.errorMsg}>Contraseña maximo 25 caracteres</Text> }
-            <Controller
+            )}
+            name="password"
+            defaultValue={defaultValues.password}
+          />
+          {errors.password?.type === 'required' && (
+            <Text style={styles.errorMsg}>Contraseña requerida</Text>
+          )}
+          {errors.password?.type === 'minLength' && (
+            <Text style={styles.errorMsg}>Contraseña minimo 8 caracteres</Text>
+          )}
+          {errors.password?.type === 'maxLength' && (
+            <Text style={styles.errorMsg}>Contraseña maximo 25 caracteres</Text>
+          )}
+          <Controller
             control={control}
-             render={({field: {onChange, onBlur, value}}) => (
+            render={({ field: { onChange, onBlur, value } }) => (
               <>
                 <Text>Repetir Contraseña</Text>
-                <TextInput style={styles.input}
-                onBlur={onBlur}
-                onChangeText={onChange}
-                value={value}
-                secureTextEntry={true}/>
+                <TextInput
+                  style={styles.input}
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                  secureTextEntry={true}
+                />
               </>
-              )}
-             name="password2"
-             defaultValue={defaultValues.password2}
-            />
-            </SafeAreaView>
-            <Button title="Adherite" style={styles.buttonE}  onPress={handleSubmit(submit)}/>
-          </View>
-        <View style={styles.divSesion}>
-          <Text style={styles.sesion}>¿Ya sos parte del club?
-            <Text style={styles.initSesion}>¡iniciar sesión!</Text>
-          </Text>
-        </View>
+            )}
+            name="password2"
+            defaultValue={defaultValues.password2}
+          />
+        </SafeAreaView>
+        <Button
+          title="Adherite"
+          style={styles.buttonE}
+          onPress={handleSubmit(submit)}
+        />
+      </View>
+      <View style={styles.divSesion}>
+        <Text style={styles.sesion}>
+          ¿Ya sos parte del club?
+          <Text style={styles.initSesion}>¡iniciar sesión!</Text>
+        </Text>
+      </View>
     </View>
   );
 };
